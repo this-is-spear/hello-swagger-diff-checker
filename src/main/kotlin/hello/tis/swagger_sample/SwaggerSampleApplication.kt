d6998cafb1ecdf5abc30ba55c6ca940b3abf277b
this-is-spear <92219795+this-is-spear@users.noreply.github.com>
2025-01-13 21:47:56 +0900

    init

---
+++ src/main/kotlin/hello/tis/swagger_sample/SwaggerSampleApplication.kt
@@ -0,0 +1,11 @@
+package hello.tis.swagger_sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwaggerSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwaggerSampleApplication>(*args)
+}
---
+++ src/test/kotlin/hello/tis/swagger_sample/SwaggerSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package hello.tis.swagger_sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwaggerSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
